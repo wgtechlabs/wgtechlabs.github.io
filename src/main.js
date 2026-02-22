0cab433b3c1208b66faa31bd1d0d2b67cda99e49
Waren Gonzaga <opensource@warengonzaga.com>
2026-02-22 18:18:10 +0800

    📦 new: initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <div class="accent"></div>
+  <div class="content">
+    <h1 class="title">WG Technology Labs</h1>
+  </div>
+`;
